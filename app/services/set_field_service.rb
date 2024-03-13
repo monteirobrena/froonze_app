356e660dfb21c6fb0b572b1edf2dd299b85582fb
Brena Monteiro <monteirobrena@gmail.com>
2024-03-13 01:43:27 +0000

    Refactor: Service changes to accept any field_name part of model attributes.

--- app/services/set_field_service.rb
+++ app/services/set_field_service.rb
@@ -1,22 +1,11 @@
 class SetFieldService
   def call(customer:, field_name:, new_value:)
     ActiveRecord::Base.transaction do
-      case field_name
-      when 'first_name'
-        customer.first_name = new_value.strip
-      when 'last_name'
-        customer.last_name = new_value.strip
-      when 'phone'
-        customer.phone = new_value.strip
-      when 'title'
-        customer.title = new_value.strip
-      when 'role'
-        customer.role = new_value.strip
-      when 'score'
-        customer.score = new_value.to_i
-      else
-        return { error: "Unknown field: '#{field_name}'" }
-      end
+      return { error: "Unknown field: '#{field_name}'" } unless Customer.attribute_names.include?(field_name)
+
+      customer[field_name] = new_value.to_i if Customer.attribute_types[field_name].type == :integer
+      customer[field_name] = new_value.strip if Customer.attribute_types[field_name].type == :string
+
       customer.save!
 
       return {}
--- spec/services/set_field_service_spec.rb
+++ spec/services/set_field_service_spec.rb
@@ -1,21 +1,74 @@
 describe SetFieldService do
   let(:set_field_service) { described_class }
 
-  let(:first_name)  { ' Rick ' }
-  let(:email)       { 'rick.grimes@twd.com' }
-  let(:customer)    { Customer.build({ first_name: first_name, email: email }) }
+  let(:first_name) { ' Rick ' }
+  let(:last_name)  { ' Grimes  ' }
+  let(:phone)      { '+55 33 991 027 150' }
+  let(:title)      { " The Ones Who Live " }
+  let(:role)       { " Former sheriff's deputy " }
+  let(:score)      { "100" }
+  let(:email)      { 'rick.grimes@twd.com' }
+  let(:customer)   { Customer.build({ first_name: first_name, email: email }) }
 
   describe '#call' do
-    context 'create customer with service' do
-      it 'should change the first_name' do
-        call_result = set_field_service.new.call(
-          customer: customer,
-          field_name: 'first_name',
-          new_value: first_name
-        )
-        expect(call_result).to be_empty
-        expect(customer.first_name).to eq(first_name.strip)
-      end
+    it 'should change the first_name' do
+      call_result = set_field_service.new.call(
+        customer: customer,
+        field_name: 'first_name',
+        new_value: first_name
+      )
+      expect(call_result).to eql({})
+      expect(customer.first_name).to eq(first_name.strip)
+    end
+
+    it 'should change the last_name' do
+      call_result = set_field_service.new.call(
+        customer: customer,
+        field_name: 'last_name',
+        new_value: last_name
+      )
+      expect(call_result).to eql({})
+      expect(customer.last_name).to eq(last_name.strip)
+    end
+
+    it 'should change the phone' do
+      call_result = set_field_service.new.call(
+        customer: customer,
+        field_name: 'phone',
+        new_value: phone
+      )
+      expect(call_result).to eql({})
+      expect(customer.phone).to eq(phone.strip)
+    end
+
+    it 'should change the title' do
+      call_result = set_field_service.new.call(
+        customer: customer,
+        field_name: 'title',
+        new_value: title
+      )
+      expect(call_result).to eql({})
+      expect(customer.title).to eq(title.strip)
+    end
+
+    it 'should change the role' do
+      call_result = set_field_service.new.call(
+        customer: customer,
+        field_name: 'role',
+        new_value: role
+      )
+      expect(call_result).to eql({})
+      expect(customer.role).to eq(role.strip)
+    end
+
+    it 'should change the score' do
+      call_result = set_field_service.new.call(
+        customer: customer,
+        field_name: 'score',
+        new_value: score
+      )
+      expect(call_result).to eql({})
+      expect(customer.score).to eq(score.to_i)
     end
   end
 end
